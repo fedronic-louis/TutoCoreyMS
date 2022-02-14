7e339a4d9cf52f156e133ed310fbe008d62d2c27
Louis <lhfedronic@gmail.com>
2022-02-14 09:34:26 +0100

    The tuto is here: https://www.youtube.com/watch?v=UmljXZIypDc&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=1&ab_channel=CoreySchafer

---
+++ django_project/blog/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+# Plus besoin maintenant quee nous avons  installer et chargé la base de données
+# https://www.youtube.com/watch?v=aHC3uTkT9r8&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=6&ab_channel=CoreySchafer
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27,2018'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28,2018'
+#     }
+# ]
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    # return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
