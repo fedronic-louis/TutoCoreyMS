70eb01981ca633261835ad616a0a1811c08754af
Louis <lhfedronic@gmail.com>
2022-02-15 14:35:59 +0100

    Profil established, now our images are in media folder

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,9 +125,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Media root permet de donnée l'acces aux médias au niveau du systeme
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Tandis que le Media Url permet de donnée l'acces des médias  cotés browser
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATES_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+
