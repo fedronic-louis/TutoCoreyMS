dd56b77cbea66a2fc3de3d901850e6bc9af84377
Louis <lhfedronic@gmail.com>
2022-02-15 11:39:42 +0100

    Login & Logout were implemented, the profile as well, nice right ?

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
 # L'import ci-dessous permet si l'on veut créer des "forms" de "scratch", il permettra differents type de validation en allant de l'email au mot de passe en passant par l'insertion d'information correct
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from .forms import UserRegisterForm
@@ -11,8 +12,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created ! You are now able to log in !')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
